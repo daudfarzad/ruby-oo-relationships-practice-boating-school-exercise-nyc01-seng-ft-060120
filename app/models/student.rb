b83ac75f87b10e69ec44c306f019080c65ee0889
daudfarzad <daudfarzad@gmail.com>
2020-06-29 21:17:31 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,20 @@
 class Student
 
-end
+    attr_reader :first_name 
+    @@all = []
+
+    def initialize (first_name)
+        @first_name = first_name 
+        @boating_test_name = boating_test_name
+        @boating_test_status = boating_test_status
+        @instructor = instructor
+        @@all << self 
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def add_boating_test
+        BoatingTest.new 
+    end 
